24adfa9043e533a6339d556edb150a1c886d0e9e
Nick Brown <nickrossbrown@gmail.com>
2018-08-03 15:42:08 -0700

    add form on index.html that collects user input information

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-
+  $("userInput").submit(function(event) {
+    event.preventDefault();
 
   });
 });
