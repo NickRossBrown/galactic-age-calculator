77a0e4f58f58df8a8fdf87f5539bae60c3fd0ab6
Nick Brown <nickrossbrown@gmail.com>
2018-08-03 18:11:27 -0700

    5 methods for GalaticAge class with one knownfailing method for constructor defaults

--- spec/galaticAge-spec.js
+++ spec/galaticAge-spec.js
@@ -1,14 +1,45 @@
 import { GalaticAge } from './../src/galaticAge.js';
 
 describe ('GalaticAge', function () {
-  let earthAge;
-  let glacticAge;
+  let person;
+  let test;
 
     beforeEach(function() {
+      person = new GalaticAge ("Nick",9,99,9999,"sun")
 
     });
 
-    it ('state user given name and age', function() {
-    expect().toEqual("Nick",28);
+    it ('displays user given name and age', function() {
+      test = new GalaticAge("Nick",1,11,111,"earf");
+      expect(test.name).toEqual("Nick");
+      expect(test.planet).toEqual("earf");
   });
+
+    it ('state user birthday on earth', function() {
+      test = new GalaticAge("Nick",1,11,111,"earf");
+      expect(test.month).toEqual(1);
+      expect(test.day).toEqual(11);
+      expect(test.year).toEqual(111);
+  });
+
+    it ('store and return todays date', function() {
+      let testDate = new Date;
+      console.log(test.todayDate.getDate())
+      console.log(test.todayDate.getMonth())
+      console.log(test.todayDate.getFullYear())
+
+      expect(test.todayDate.getDate()).toEqual(testDate.getDate());
+      expect(test.todayDate.getMonth()).toEqual(testDate.getMonth());
+      expect(test.todayDate.getFullYear()).toEqual(testDate.getFullYear());
+  });
+
+  it ('should test yearsToDays(years) to see if it returns the number of days for the number of years inputted', function() {
+    console.log(person)
+    let years = 10;
+      console.log(person.yearsToDays(10))
+    let days = person.yearsToDays(10);
+    expect(days).toEqual(3650);
+  });
+
+
 });
--- src/galaticAge.js
+++ src/galaticAge.js
@@ -1,9 +1,56 @@
 export class GalaticAge {
 
-  constructor(name, age) {
+  constructor(name, month, day, year, planet) {
     this.name = name
-    this.age = age
+    this.day = day
+    this.month = month
+    this.year = year
+    this.planet = planet
+    this.todayDate = new Date();
   }
+    //
+//want to add default values to constucter
+
+  // function defaults (def){
+  //     this.def = {name:"defaultName",
+  //     day:99,
+  //     month:99,
+  //     year:999,
+  //     planet:"earth"
+  //   };
+  // };
+
+
+   yearsToDays(years){
+     years = years * 365
+     return years
+   }
+
+    monthToDays(month){
+    let i = month
+    let totalDays = 0
+    while (i > 0) {
+      totalDays=+totalDays
+      i-=1
+    }
+    return totalDays
+  }
+
+  daysInMonth (month) {
+    let date = new Date(1990, month, 0).getDate();
+    return date
+   }
+
+   daysToHours(days){
+     days = days * 24
+     return days
+   }
+
+   hoursToSec(hours){
+   hours = hours * 60
+   return hours
+   }
+
 
 
 }
