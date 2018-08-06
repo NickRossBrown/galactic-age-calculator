61eebaf4a5f75467b51ffe1d0e98334936fc7b55
Nick Brown <nickrossbrown@gmail.com>
2018-08-06 08:05:18 -0700

    methods for life expectancy

--- src/lifeExpectancy.js
+++ src/lifeExpectancy.js
@@ -8,4 +8,68 @@ export class LifeExpectancy {
     this.slope = []
   }
 
+
+  slope(x1,y1,x2,y2) {
+    let slope = (y2 - y1) / (x2 - x1)
+    return slope.toFixed(2)
+  }
+
+  findY2(x1,x2,y1,slope) {
+    let y2 = ((x2-x1)*slope)+y1
+    return y2
+  }
+
+  loopObject(obj){
+    obj = this.maleExpectancyValues
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+          console.log(key + " -> " + obj[key]);
+          const k = parseInt(key);
+          const v = obj[key]
+          const slope = [k,(v+k).toFixed(2)]
+          this.xyCoor.push(slope)
+          console.log("y values---"+this.xyCoor)
+      }
+     }
+   }
+
+   loopArray(array1){
+     for ( let i = 0; i<array1.length-1 ; i++) {
+       let x1 = array1[i][0]
+       let y1 = parseFloat(array1[i][1])
+       let xx2 = array1[i+1][0]
+       let yy2 = parseFloat(array1[i+1][1])
+
+       let m = this.slope(x1,y1,xx2,yy2)
+       this.slope.push(m)
+       console.log("MMM"+m)
+
+     }
+   }
+
+   lifeCalc(){
+     console.log(this.xyCoor.length)
+     for (let i = 0; i< this.xyCoor.length ; i++) {
+       console.log("here")
+       console.log(this.xyCoor[i][0])
+       if (this.age >= this.xyCoor[i][0] && this.age < this.xyCoor[i+1][0]){
+
+         console.log(parseFloat(this.xyCoor[i][1]))
+         let slope = this.slope[i]
+         let x1 = this.xyCoor[i][0]
+         let x2 = this.age
+         let y1 = parseFloat(this.xyCoor[i][1])
+         const age = this.findY2(x1,x2,y1,slope)
+         console.log(age)
+         return age
+       }
+     }
+   }
+
+  overAverageLifeSpan(age){
+    if (age > 76.5){
+      return age - 76.5
+    } else return 76.5 - age
+  }
+
 }
